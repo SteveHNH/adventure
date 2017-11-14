9cdf85749ad761cfb470d0f9d17bc0564cb63189
Stephen Adams <tsadams@gmail.com>
2017-11-14 06:53:54 -0500

    add weapon class and weapons

--- character.py
+++ character.py
@@ -52,17 +52,50 @@ class Inventory:
     def removeFromInv(item):
         return
 
-class Item:
+
+class Item():
     """
     Handle items in the game inluding gold, weapons, and items that can be used
     for a specific purpose.
     """
 
-    def __init__(self, name, weight, value, type_):
+    def __init__(self, name, description, value):
 
         self.name = name
-        self.weight = weight
+        self.description = description
         self.value = value
-        self.type_ = type_
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description)
+
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name="Gold",
+                         description="A round coind with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
 
 
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+
+
+class Rock(Weapon):
+    def __init__(self):
+        super().__init__(name="Rock",
+                         description="A fist-sized rock suitable for bludgeoning",
+                         value=0,
+                         damage=5)
+
+
+class Dagger(Weapon):
+    def __init__(self):
+        super().__init__(name="Dagger",
+                         description="A small dagger with some rust on the blade.",
+                         value=10,
+                         damage=10)
